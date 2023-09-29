87ec43edcef47143ae7def71d1a9e26b5ce7ab74
Thanadech Sukprasong <66402857+Tanukicho@users.noreply.github.com>
2023-09-29 10:59:37 +0700

    Update auto_click_i.py

--- auto_click_i.py
+++ auto_click_i.py
@@ -28,7 +28,7 @@ def on_press(key):
 
 
 def display_controls():
-    print("// AutoClicker by iSayChris")
+    print("// AutoClicker by iSayChris") # credit by iSayChris
     print("// - Settings: ")
     print("\t delay = " + str(delay) + ' sec' + '\n')
     print("// - Controls:")
